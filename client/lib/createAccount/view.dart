096039bfac1a64122f6a4992e43330268741ab54
Kipp Adrich Joseph <kippadrichjoseph@hotmail.com>
2024-02-13 17:32:45 -0700

    create account/log in

--- client/lib/createAccount/view.dart
+++ client/lib/createAccount/view.dart
@@ -1,61 +1,66 @@
-import 'package:client/login/view_model.dart';
+import 'package:client/login/view.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'view_model.dart';
-
-//import 'package:client/screens/home.dart';
-import 'package:client/home/view.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-void main() => runApp(MaterialApp(home: createAccount(),));
+void main() => runApp(MaterialApp(home: CreateAccount(),));
 
-class createAccount extends StatefulWidget {
+class CreateAccount extends StatefulWidget {
   @override
-  _createAccountState createState() => _createAccountState(); // add state
+  _CreateAccountState createState() => _CreateAccountState();
 }
 
-class _createAccountState extends State<createAccount> { // state / create account
+class _CreateAccountState extends State<CreateAccount> {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController serverIPController = TextEditingController();
+  final TextEditingController FirstNameController = TextEditingController();
+  final TextEditingController LastNameController = TextEditingController();
   bool isPasswordVisible = false;
 
-  _login(BuildContext context, TextEditingController usernameController) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (_) => WelcomePage(
-          loggedInUsername: usernameController.text.trim(),
-          serverIP: serverIPController.text.trim(),
-        ),
-      ),
-    );
+
+  late IO.Socket _socket; // Declare the _socket variable
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeSocket();
   }
 
-  _showAccountCreatedDialog(BuildContext context) {
+
+
+  void _initializeSocket() {
+    // Assuming serverIPController contains the full server IP including protocol and port
+    String serverIP = serverIPController.text.isNotEmpty ? "http://${serverIPController.text}" : 'http://192.168.56.1:3000';
+
+    // Initialize socket connection
+    _socket = IO.io(serverIP, <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+    _socket.connect();
+
+    // Optional: Setup listeners here if needed, similar to HomeScreen
+    _socket.onConnect((_) {
+      print('Connected to socket server at $serverIP');
+    });
+    _socket.onConnectError((data) {
+      print('Connect Error: $data');
+    });
+  }
+
+  void _showAccountCreatedDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          backgroundColor: Colors.green, // Set the background color to green
-          title: Text(
-            "Account Created!",
-            style: TextStyle(color: Colors.white), // Set text color to white
-          ),
-          content: Text(
-            "Your account has been successfully created.",
-            style: TextStyle(color: Colors.white), // Set text color to white
-          ),
+          backgroundColor: Colors.green,
+          title: Text("Account Created!", style: TextStyle(color: Colors.white)),
+          content: Text("Your account has been successfully created.", style: TextStyle(color: Colors.white)),
           actions: [
             TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
-              },
-              child: Text(
-                "OK",
-                style: TextStyle(color: Colors.white), // Set text color to white
-              ),
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("OK", style: TextStyle(color: Colors.white)),
             ),
           ],
         );
@@ -65,10 +70,11 @@ class _createAccountState extends State<createAccount> { // state / create accou
 
   void _createAccount(BuildContext context) {
     String username = usernameController.text.trim();
+    String firstname = FirstNameController.text.trim();
+    String lastname = LastNameController.text.trim();
     String password = passwordController.text.trim();
 
     if (username.isEmpty || password.isEmpty) {
-      // Show an error message if either username or password is empty
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text("Please enter both username and password."),
@@ -76,174 +82,146 @@ class _createAccountState extends State<createAccount> { // state / create accou
         ),
       );
     } else {
-      // If both fields are filled, show the "Account Created" dialog
+      _socket.emit('createaccount', {'userID': username, 'Firstname': firstname, 'Lastname': lastname, 'password': password});
       _showAccountCreatedDialog(context);
-      // Add logic to create account if needed
     }
   }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: <Widget>[
-              // Add the AppBar with a back button
-              AppBar(
-                backgroundColor: Colors.white,
-                elevation: 0, // Remove shadow
-                leading: IconButton(
-                  icon: Icon(Icons.arrow_back_ios_new_outlined, color: Colors.green),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ),
-
-              Container(
-                height: 300,
-                color: Colors.white,
-                child: Align(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              backgroundColor: Colors.white,
+              elevation: 0,
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back_ios_new_outlined, color: Colors.green),
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => Homepage()));
+                }
+                  ),
+                  ),
+                  Container(
+                  child: Align(
                   alignment: Alignment.topCenter,
                   child: Padding(
-                    padding: EdgeInsets.only(top: 50),
-                    child: Column(
-                      children: [
-                        Image.asset(
-                          'assets/Hi.png',
-                          width: 200,
-                          height: 220,
-                        ),
-                        SizedBox(height: 10),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 20),
-              Text(
-                'Create your Account',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Create your account to start collaborating',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'Roboto',
-                  fontSize: 12,
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-
-              // For enter username:
-              SizedBox(height: 20),
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Column(
+                  padding: EdgeInsets.only(top: 50),
+                  child: Column(
                   children: [
-                    TextFormField(
-                      controller: usernameController,
-                      decoration: InputDecoration(
-                        labelText: 'Username',
-                        hintText: 'Create Username',
-                        labelStyle: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                        ),
-                        prefixIcon: Icon(Icons.person, color: Colors.black, size: 18),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black, width: 1.5),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ),
-
-                    // for enter password:
-                    SizedBox(height: 20),
-                    TextFormField(
-                      controller: passwordController,
-                      obscureText: !isPasswordVisible,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        hintText: 'Create password',
-                        labelStyle: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                        ),
-                        prefixIcon: Icon(Icons.lock, color: Colors.black, size: 18),
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              isPasswordVisible = !isPasswordVisible;
-                            });
-                          },
-                          child: Icon(
-                            isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                            color: Colors.black,
-                            size: 18,
-                          ),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black, width: 1.5),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ),
-
-                    // For Create Account button:
-                    SizedBox(height: 30),
-                    MaterialButton(
-                      onPressed: () {
-                        _createAccount(context);
-                      },
-                      height: 45,
-                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                      shape: RoundedRectangleBorder(
+                  Image.asset('assets/Hi.png', width: 200, height: 220),
+                  SizedBox(height: 10),
+                  ],
+                  ),
+                  ),
+                  ),
+                  ),
+                  SizedBox(height: 20),
+                  Text('Create your Account', style: TextStyle(color: Colors
+                .black, fontFamily: 'Roboto', fontSize: 18, fontWeight: FontWeight.bold)),
+            Text('Create your account to start collaborating', style: TextStyle(color: Colors.black, fontFamily: 'Roboto', fontSize: 12, fontWeight: FontWeight.normal)),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      hintText: 'Create Username',
+                      prefixIcon: Icon(Icons.person, color: Colors.black, size: 18),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
                         borderRadius: BorderRadius.circular(10),
                       ),
-                      color: Colors.lightGreen,
-                      child: Text(
-                        'Create Account',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 1.5),
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
-                    SizedBox(height: 20), // Added bottom padding to the container
-                  ],
-                ),
+                  ),
+                  TextFormField(
+                    controller: FirstNameController,
+                    decoration: InputDecoration(
+                      labelText: 'First Name',
+                      hintText: 'Enter First Name',
+                      prefixIcon: Icon(Icons.person, color: Colors.black, size: 18),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 1.5),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  TextFormField(
+                    controller: LastNameController,
+                    decoration: InputDecoration(
+                      labelText: 'Last Name',
+                      hintText: 'Enter Last Name',
+                      prefixIcon: Icon(Icons.person, color: Colors.black, size: 18),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 1.5),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: !isPasswordVisible,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Create password',
+                      prefixIcon: Icon(Icons.lock, color: Colors.black, size: 18),
+                      suffixIcon: GestureDetector(
+                        onTap: () => setState(() => isPasswordVisible = !isPasswordVisible),
+                        child: Icon(isPasswordVisible ? Icons.visibility : Icons.visibility_off, color: Colors.black, size: 18),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 1.5),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 30),
+                  MaterialButton(
+                    onPressed: () => _createAccount(context),
+                    height: 45,
+                    color: Colors.lightGreen,
+                    child: Text('Create Account', style: TextStyle(color: Colors.white)),
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  ),
+                  SizedBox(height: 20),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
+  }
+  @override
+  void dispose() {
+    _socket.disconnect();
+    super.dispose();
   }
 }
