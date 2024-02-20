b5399f818c5118156b3d707c3d364bf6be9eb437
Kipp Adrich Joseph <kippadrichjoseph@hotmail.com>
2024-02-20 08:37:44 -0700

    calendar add

--- client/lib/collaborate/Calendar.dart
+++ client/lib/collaborate/Calendar.dart
@@ -7,68 +7,181 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
-  CalendarFormat _calendarFormat = CalendarFormat.twoWeeks; // Initial format
+  CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+  bool _showFloatingButton = false; // To control the visibility of the FAB
+
+  final Map<DateTime, List<String>> _events = {
+    DateTime.utc(2024, 2, 20): ['Event 1', 'Event 2'], // Example events
+    // Add more events here
+  };
+
+  List<String> _getEventsForDay(DateTime day) {
+    return _events[DateTime(day.year, day.month, day.day)] ?? [];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TableCalendar(
-        firstDay: DateTime.utc(2010, 10, 16),
-        lastDay: DateTime.utc(2030, 3, 14),
-        focusedDay: _focusedDay,
-        calendarFormat: _calendarFormat,
-        availableCalendarFormats: const {
-          CalendarFormat.month: 'Month', // Only Month format is available
-        },
-        headerStyle: HeaderStyle(
-          titleCentered: true, // Center the title
-          formatButtonDecoration: BoxDecoration(
-            color: Colors.green,
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          formatButtonTextStyle: TextStyle(color: Colors.white),
-        ),
-        calendarStyle: CalendarStyle(
-          // Customize the following properties as needed
-          defaultDecoration: BoxDecoration(
-            shape: BoxShape.circle,
-          ),
-          weekendDecoration: BoxDecoration(
-            shape: BoxShape.circle,
-          ),
-          selectedDecoration: BoxDecoration(
-            color: Colors.green,
-            shape: BoxShape.circle,
-          ),
-          todayDecoration: BoxDecoration(
-            color: Colors.green.withOpacity(0.5),
-            shape: BoxShape.circle,
-          ),
-        ),
-        selectedDayPredicate: (day) {
-          return isSameDay(_selectedDay, day);
-        },
-        onDaySelected: (selectedDay, focusedDay) {
-          if (!isSameDay(_selectedDay, selectedDay)) {
-            setState(() {
-              _selectedDay = selectedDay;
+      appBar: AppBar(title: Text('Calendar')),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            availableCalendarFormats: const {
+              CalendarFormat.month: 'Month',
+            },
+            headerStyle: HeaderStyle(
+              titleCentered: true,
+              formatButtonDecoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              formatButtonTextStyle: TextStyle(color: Colors.white),
+            ),
+            calendarStyle: CalendarStyle(
+              defaultDecoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+              weekendDecoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+              selectedDecoration: BoxDecoration(
+                color: Colors.green,
+                shape: BoxShape.circle,
+              ),
+              todayDecoration: BoxDecoration(
+                color: Colors.green.withOpacity(0.5),
+                shape: BoxShape.circle,
+              ),
+            ),
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+                _showFloatingButton = true; // Show the FAB when a day is selected
+              });
+            },
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
               _focusedDay = focusedDay;
-            });
-          }
-        },
-        onFormatChanged: (format) {
-          if (_calendarFormat != format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          }
-        },
-        onPageChanged: (focusedDay) {
-          _focusedDay = focusedDay;
-        },
+            },
+            eventLoader: _getEventsForDay,
+            calendarBuilders: CalendarBuilders(
+              markerBuilder: (context, date, events) {
+                if (events.isNotEmpty) {
+                  return Positioned(
+                    right: 1,
+                    bottom: 1,
+                    child: _buildEventsMarker(date, events),
+                  );
+                }
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _selectedDay != null ? _getEventsForDay(_selectedDay!).length : 0,
+              itemBuilder: (context, index) {
+                if (_selectedDay != null) {
+                  final events = _getEventsForDay(_selectedDay!);
+                  return ListTile(
+                    title: Text(events[index]),
+                  );
+                } else {
+                  return Container(); // No selected day
+                }
+              },
+            ),
+          ),
+        ],
       ),
+      floatingActionButton: _showFloatingButton ? FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: () => _showAddEventDialog(context),
+        child: Icon(Icons.add),
+      ) : null,
+    );
+  }
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: Colors.green[700],
+      ),
+      width: 16.0,
+      height: 16.0,
+      child: Center(
+        child: Text(
+          '${events.length}',
+          style: TextStyle().copyWith(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showAddEventDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Create Event"),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Name of event",
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Share to user (optional)",
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Share to server (optional)",
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Create'),
+              onPressed: () {
+                // Here you can add your logic to handle event creation
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
