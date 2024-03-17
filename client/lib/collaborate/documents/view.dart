0c8ed8c185158a94a81b42396b540b1af6d8a39b
Jordan Lai <jlai.github@gmail.com>
2024-03-17 14:33:42 -0600

    before document implementation

--- client/lib/collaborate/documents/view.dart
+++ client/lib/collaborate/documents/view.dart
@@ -6,13 +6,29 @@ class DocumentsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: const Text('Documents'),
-      backgroundColor: Colors.green,
-    ),
-      body: Center(
-        // Replace 'path_to_your_image' with your actual image path
-        child: Image.asset('assets/FileEdit.webp'),
+      appBar: AppBar(
+        title: const Text('Documents'),
+        backgroundColor: Colors.green,
+      ),
+      // Background image
+      body: new Stack(
+        children: <Widget>[
+          Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage("assets/FileEdit.webp"),
+                colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
+              ),
+            ),
+          ),
+        ],
+      ),
+      // New document button
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: () {
+        },
+        child: Icon(Icons.add, color: Colors.white),
       ),
     );
   }
