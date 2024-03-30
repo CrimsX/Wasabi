fe8e48e9ce959dc2bf8c68e8b1c99ede03cf6f7d
Jordan Lai <jlai.github@gmail.com>
2024-03-29 18:42:43 -0600

    revert back to hash for pass

--- server/database/login.js
+++ server/database/login.js
@@ -13,11 +13,11 @@ export async function logIn(data) {
 
         // Fetch the password for the given userID
         const [rows] = await pool.query('SELECT Pass FROM client WHERE UserID = ?', [userID]);
-        console.log(rows[0]["Pass"])
-        console.log(password);
-        console.log(bcrypt.compareSync(password, rows[0]["Pass"]))
-        //if (bcrypt.compareSync(password, rows[0]["Pass"])) {
-        if (password === rows[0]["Pass"]) {
+        //console.log(rows[0]["Pass"])
+        //console.log(password);
+        //console.log(bcrypt.compareSync(password, rows[0]["Pass"]))
+        if (bcrypt.compareSync(password, rows[0]["Pass"])) {
+        //if (password === rows[0]["Pass"]) {
         // If a row is found, the password matches
             return { success: true, message: "Login successful" };
         } else {
